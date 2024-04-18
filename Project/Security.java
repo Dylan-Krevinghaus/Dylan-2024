5097869d1fff52685723ec1d83420eadf7ee814e
Dylan-Krevinghaus <dillpenguin@gmail.com>
2024-04-18 14:16:26 -0400

    Security checking started

---
+++ Project/Security.java
@@ -0,0 +1,18 @@
+public class Security {
+    String password;
+    int whileFIX;
+    public boolean firstTime()
+    {
+    }
+    public String generatePassword()
+    {
+    }
+    public boolean checkPassword(String password)
+    {
+        whileFIX = 0;
+        while(whileFIX == 0)
+        {
+        }
+        return false;
+    }
+}
