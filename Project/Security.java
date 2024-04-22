c7dd763aed4908063a86686966b6e6eba8c72713
Dylan-Krevinghaus <dillpenguin@gmail.com>
2024-04-22 12:07:55 -0400

    Security

--- Project/Security.java
+++ Project/Security.java
@@ -1,11 +1,22 @@
+import java.io.BufferedReader;
+
 public class Security {
     String password;
     int whileFIX;
+    String file = "ReadingThings";
+    // BufferedReader reader = new BufferedReader(file);
     public boolean firstTime()
     {
+        //check if it is the first time the user entered
+        catch(RecordHeaderName ex)
+        {
+            ex.
+        }
     }
     public String generatePassword()
     {
+        //Make 5 lower case and 5 upper case
+        //Make it start and end with a single digit number
     }
     public boolean checkPassword(String password)
     {
